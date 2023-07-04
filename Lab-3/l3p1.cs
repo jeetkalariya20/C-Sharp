1cf042d0e36457c931b6698fbd26a6e62a378766
Jeet Kalariya <93392236+jeetkalariya20@users.noreply.github.com>
2023-07-04 21:40:00 +0530

    Add files via upload

---
+++ Lab-3/l3p1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candidate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Candidate c1= new Candidate();
+            c1.GetCandidateDetails();
+            c1.DisplayCandidateDetails();
+
+            Console.ReadLine();
+        }
+    }
+    class Candidate
+    {
+        int ID;
+        String Name;
+        int Age;
+        int Weight;
+        int Height;
+
+        public void GetCandidateDetails()
+        {
+            Console.WriteLine("Enter ID : ");
+            ID=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Name : ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Age : ");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Weight : ");
+            Weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Height : ");
+            Height = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void DisplayCandidateDetails()
+        {
+            Console.WriteLine("ID : " + ID);
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Age : " + Age);
+            Console.WriteLine("Weight : " + Weight);
+            Console.WriteLine("Height : " + Height);
+        }
+    }
+}
