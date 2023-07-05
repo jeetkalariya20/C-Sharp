281aa2cacae1fbc8309b8311d04d066b91e1b210
Jeet Kalariya <93392236+jeetkalariya20@users.noreply.github.com>
2023-07-05 19:54:01 +0530

    Add files via upload

--- Lab-1/l1p5.cs
+++ Lab-1/l1p5.cs
@@ -6,42 +6,44 @@ using System.Threading.Tasks;
 
 namespace Area
 {
-    internal class Area
+    internal class l1p5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.Square \n 2.Rectangle \n 3.Circle");
-            double ch;
-            ch=Convert.ToInt32(Console.ReadLine());
+           
+                Console.WriteLine("1.Square \n 2.Rectangle \n 3.Circle");
+                double ch;
+                ch = Convert.ToInt32(Console.ReadLine());
 
-            switch (ch)
-            {
-                case 1:
-                    int a;
-                    Console.WriteLine("Enter a : ");
-                    a= Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Area of square is " + (a * a));
-                    break;
+                switch (ch)
+                {
+                    case 1:
+                        int a;
+                        Console.WriteLine("Enter a : ");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Area of square is " + (a * a));
+                        break;
 
-                case 2:
-                    int l, b;
-                    Console.WriteLine("Enter l : ");
-                    l = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Enter b : ");
-                    b = Convert.ToInt32(Console.ReadLine());
+                    case 2:
+                        int l, b;
+                        Console.WriteLine("Enter l : ");
+                        l = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter b : ");
+                        b = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine("Area of rectangle is : " + (l * b) / 2);
-                    break;
+                        Console.WriteLine("Area of rectangle is : " + (l * b));
+                        break;
 
-                case 3:
-                    int r;
-                    Console.WriteLine("Enter r : ");
-                    r=Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Area of circle is : " + (3.14 * r * r));
-                    break;
+                    case 3:
+                        int r;
+                        Console.WriteLine("Enter r : ");
+                        r = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Area of circle is : " + (3.14 * r * r));
+                        break;
+                }
+
+                Console.ReadLine();
             }
-
-            Console.ReadLine();
-        }
+        
     }
 }
