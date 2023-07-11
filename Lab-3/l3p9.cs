23160767838a8d7f3de40978a3ed2d692149aeb6
Jeet Kalariya <93392236+jeetkalariya20@users.noreply.github.com>
2023-07-11 19:48:09 +0530

    Add files via upload

---
+++ Lab-3/l3p9.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l3p9
+{
+    internal class l3p9
+    {
+        static void Main(string[] args)
+        {
+            Calculate c = new Calculate();
+            c.calculateROI();
+            Console.ReadLine();
+        }
+    }
+    class BankAccount
+    {
+        public int AccountNumber;
+        public String UserName;
+        public String Email;
+        public String Type;
+        public double Balance;
+
+        public BankAccount()
+        {
+            Console.WriteLine("Enter Account Number : ");
+            this.AccountNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter User Name : ");
+            this.UserName = Console.ReadLine();
+            Console.WriteLine("Enter Email : ");
+            this.Email = Console.ReadLine();
+            Console.WriteLine("Enter Type : ");
+            this.Type = Console.ReadLine();
+            Console.WriteLine("Enter Balance : ");
+            this.Balance = Convert.ToDouble(Console.ReadLine());
+        }
+
+    }
+    class Calculate : BankAccount
+    {
+        public double TimePeriod;
+        public double RateOfIntrest;
+
+        public Calculate()
+            : base()
+        {
+            Console.WriteLine("Enter Time Period : ");
+            this.TimePeriod = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Rate Of Intrest : ");
+            this.RateOfIntrest = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void calculateROI()
+        {
+            Console.WriteLine("ROI : " + (Balance * RateOfIntrest * TimePeriod) / 100);
+        }
+    }
+}
